2054bf91d2634a539cf51950f3e56cb1f8bf2963
Masataka Yakura <masataka.yakura@gmail.com>
2015-04-30 17:57:59 +0900

    First cut of Firefox extension support

---
+++ _curcuma_firefox.js
@@ -0,0 +1,15 @@
+const data = require('sdk/self').data
+const pageMod = require('sdk/page-mod')
+
+pageMod.PageMod({
+  include: "https://chromium.googlesource.com/*",
+  contentScriptFile: [
+    data.url('../curcuma_utils.js'),
+    data.url('../curcuma_autolink.js'),
+    data.url('../curcuma_preview.js'),
+    data.url('../curcuma_title.js'), 
+  ],
+  contentStyleFile: [
+    data.url('../curcuma_preview.css')
+  ]
+})
