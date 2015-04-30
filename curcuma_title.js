512f79d80594580f2472e09b7dee109e97a5de82
Masataka Yakura <masataka.yakura@gmail.com>
2015-04-30 19:53:01 +0900

    update c_title.js to use const

--- curcuma_title.js
+++ curcuma_title.js
@@ -3,14 +3,13 @@
 // modifying changeset title to include summary
 // original: [hash] - [repo/dir] - Git at Google
 // modified: [repo/dir] - [shorthash] -- [summary]
-var updateTitle = function () {
-  var message = document.querySelector('.commit-message').textContent
-  // var revision = /git-svn-id: svn:\/\/.*@(\d+)/.test(message) ? message.match(/git-svn-id: svn:\/\/.*@(\d+)/)[1] : null
-  var summary = message.match(/(.*)\n/)[1]
-  var title = document.title
-  var reTitle = /([0-9a-f]+) - (.+?) /
-  var repo = reTitle.exec(title)[2]
-  var hash = reTitle.exec(title)[1].slice(0, 10)
+const updateTitle = function () {
+  const message = document.querySelector('.commit-message').textContent
+  const summary = message.match(/(.*)\n/)[1]
+  const title = document.title
+  const reTitle = /([0-9a-f]+) - (.+?) /
+  const repo = reTitle.exec(title)[2]
+  const hash = reTitle.exec(title)[1].slice(0, 10)
   document.title = repo + ' - ' + hash + ' \u2014 ' + summary
 }
 
