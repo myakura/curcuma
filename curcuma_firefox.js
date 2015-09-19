c9bdceede953a1d375e75043d481e4ea7f745735
Masataka Yakura <masataka.yakura@gmail.com>
2015-09-19 18:00:40 +0900

    update Curcuma for non-chromium googlesource.com projects
    autolinking is currently tied to crbug.com so applying it only to chromium.googlesource.com

--- curcuma_firefox.js
+++ curcuma_firefox.js
@@ -4,14 +4,20 @@ const data = require('sdk/self').data
 const pageMod = require('sdk/page-mod')
 
 pageMod.PageMod({
-  include: 'https://chromium.googlesource.com/*',
+  include: 'https://*.googlesource.com/*',
   contentScriptFile: [
     data.url('../curcuma_utils.js'),
-    data.url('../curcuma_title.js'), 
-    data.url('../curcuma_autolink.js'), 
+    data.url('../curcuma_title.js'),
     data.url('../curcuma_preview.js'),
   ],
   contentStyleFile: [
     data.url('../curcuma_preview.css'),
+  ],
+})
+
+pageMod.PageMod({
+  include: 'https://chromium.googlesource.com/*',
+  contentScriptFile: [
+    data.url('../curcuma_autolink.js'),
   ],
 })
