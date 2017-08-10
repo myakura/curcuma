12d808f482911a5cf74cedeb038c895629a596b9
Masataka Yakura <masataka.yakura@gmail.com>
2017-08-10 13:52:21 +0900

    support 'Bug: ######' style bug line

--- curcuma_autolink.js
+++ curcuma_autolink.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const autoLinkBugs = () => {
-  const reBugs = /BUG=\s*(\d+)(?:,\s*)?(\d+)?(?:,\s*)?(\d+)?(?:,\s*)?(\d+)?(?:,\s*)?(\d+)?/
+  const reBugs = /BUG[:=]\s*(\d+)(?:,\s*)?(\d+)?(?:,\s*)?(\d+)?(?:,\s*)?(\d+)?(?:,\s*)?(\d+)?/i
   const elemMessage = document.querySelector('.MetadataMessage')
   if (!reBugs.test(elemMessage.textContent)) { return }
 
@@ -9,7 +9,7 @@ const autoLinkBugs = () => {
   const bugIDs = reBugs.exec(elemMessage.textContent).slice(1).filter(v => !!v)
 
   const crbugPrefix = 'https://bugs.chromium.org/p/chromium/issues/detail?id='
-  const newBugLine = `BUG=${bugIDs.map(id => `<a class="crbug" href="${crbugPrefix + id}">${id}</a>`).join(', ')}`
+  const newBugLine = `Bug: ${bugIDs.map(id => `<a class="crbug" href="${crbugPrefix + id}">${id}</a>`).join(', ')}`
   elemMessage.innerHTML = commitMessage.replace(reBugs, newBugLine)
 }
 
